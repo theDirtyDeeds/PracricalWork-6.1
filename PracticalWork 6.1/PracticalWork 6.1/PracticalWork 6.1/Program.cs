f1f7cc93a4e1ea3a8333540f275175213a00bf82
theDirtyDeeds <ghost054rus@mail.ru>
2024-05-30 16:41:00 +0700

    Add files via upload

---
+++ PracticalWork 6.1/PracticalWork 6.1/PracticalWork 6.1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection.Metadata;
+
+namespace PracticalWork_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите действие: \n1: Запись.\n2: Чтение.");
+
+            int userChoise = Convert.ToInt32(Console.ReadLine());
+
+            switch (userChoise)
+            {
+                case 1:
+                    Recording();
+                    break;
+                case 2:
+                    Reading();
+                    break;
+            }
+        }
+
+        static string Recording()
+        {
+
+            Console.WriteLine("Введите данные нового сотрудника: ");
+
+            Console.WriteLine("ФИО: ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Возраст: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Рост: ");
+
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Дата рождения: ");
+
+            string birthDay = Console.ReadLine();
+
+            Console.WriteLine("Место рождения: ");
+
+            string birthPlace = Console.ReadLine();
+
+            int iD = 1;
+
+            if (File.Exists(@"\data.txt"))
+            {
+                iD = File.ReadAllLines(@"\data.txt").Length + 1;
+            }
+
+            string employerData = $"{iD}, {DateTime.Now:g}, {name}, {age}, {height}, {birthDay}, {birthPlace}";
+
+            string[] employer = employerData.Split(" ");
+
+            foreach (string e in employer)
+            {
+                Console.WriteLine(e);
+            }
+
+            string path = @"\data.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            if (!fileInfo.Exists) 
+            {
+                using (StreamWriter sw = new StreamWriter(path)) 
+                {
+                    sw.WriteLine(employerData);
+
+                    Console.WriteLine("Сотрудник добавлен");
+                }
+            }
+            else
+            {
+                File.AppendAllText(@"\data.txt", employerData + Environment.NewLine);
+
+                Console.WriteLine("Сотрудник добавлен");
+            }
+            
+            return employerData;
+
+        }
+        static string Reading()
+        {
+            string path = @"\data.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            if (fileInfo.Exists)
+            {
+                using (StreamReader reader = fileInfo.OpenText()) 
+                {
+                    string text = reader.ReadToEnd();
+                    
+                    Console.WriteLine(text);
+                }
+            }
+
+            return path;
+        }
+
+
+    }
+}
